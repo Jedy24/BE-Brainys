713d68e360bb5a1cdb4a87f9ea95c11fbe0ad331
Jedy24 <reihimawari27@gmail.com>
2024-01-29 10:09:24 +0700

    Maintenance code

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -11,6 +11,7 @@ use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
+use App\Notifications\OtpNotification;
 
 class RegisterController extends Controller
 {
