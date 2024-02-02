d5d44f63450e3271c98ea2a58ca6567831a2c268
Jedy24 <reihimawari27@gmail.com>
2024-02-02 11:31:06 +0700

    Email Notification

--- app/Notifications/OtpNotification.php
+++ app/Notifications/OtpNotification.php
@@ -40,7 +40,9 @@ class OtpNotification extends Notification
                 ->subject('Kode OTP untuk Verifikasi')
                 ->line('Kode OTP untuk verifikasi adalah: ' . $this->otp)
                 ->action('Verifikasi Sekarang', url('/verify-otp'))
-                ->line('Jika anda tidak meminta kode OTP, abaikan pesan ini.');
+                ->line('Jika anda tidak meminta kode OTP, abaikan pesan ini.')
+                ->salutation('Regards, Oasys Syllabus')
+                ->line('Jika Anda mengalami masalah pada tombol "Verifikasi Sekarang", salin URL dibawah pada web browser: ' . url('/verify-otp'));
     }
 
     /**
