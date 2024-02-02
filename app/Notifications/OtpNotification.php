1e4d74f76c724a51e5fe946c8c1e4877ee276dcb
Jedy24 <reihimawari27@gmail.com>
2024-02-02 11:09:47 +0700

    Revert Changes

--- app/Notifications/OtpNotification.php
+++ app/Notifications/OtpNotification.php
@@ -38,7 +38,6 @@ class OtpNotification extends Notification
     {
         return (new MailMessage)
                 ->subject('Kode OTP untuk Verifikasi')
-                ->markdown('emails.otp-verification', ['otp' => $this->otp])
                 ->line('Kode OTP untuk verifikasi adalah: ' . $this->otp)
                 ->action('Verifikasi Sekarang', url('/verify-otp'))
                 ->line('Jika anda tidak meminta kode OTP, abaikan pesan ini.');
