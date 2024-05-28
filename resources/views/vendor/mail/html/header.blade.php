76bdbb0765cc6f061349b7c55559dc854b63a5d9
Jedy24 <reihimawari27@gmail.com>
2024-05-28 22:34:55 +0700

    Mail template

---
+++ resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,12 @@
+@props(['url'])
+<tr>
+    <td class="header">
+        <a href="{{ $url }}" style="display: inline-block;">
+            @if (trim($slot) === 'Laravel')
+                <img src="{{ url('img/Logo@2x.png') }}" class="logo" alt="Brainys">
+            @else
+                <img src="https://res.cloudinary.com/dcwpj7pny/image/upload/v1716910098/Logo_2x_a1qudn.png" width="150" alt="Brainys">
+            @endif
+        </a>
+    </td>
+</tr>
