2287e134ab1157cbc2dfb8ac93dd6de6440fe36a
Nathanael Ferry Pratama <ntnferry1505@gmail.com>
2024-06-10 18:11:11 +0700

    Auto invite google

--- app/Http/Controllers/Auth/SocialiteController.php
+++ app/Http/Controllers/Auth/SocialiteController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\Http\Controllers\Api\SendInvitationController;
 use App\Http\Controllers\Controller;
+use App\Models\AutoInviteEmail;
 use Illuminate\Http\Request;
 use App\Models\User;
+use App\Models\UserInvitation;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
@@ -38,37 +41,63 @@ class SocialiteController extends Controller
         /**Mengambil data user dari library Socialite.
          * Jika gagal maka akan mengembalikan pesan error.
          */
-        try{
+        try {
             $user = Socialite::driver($provider)->stateless()->user();
-        } catch (ClientException $exception){
+        } catch (ClientException $exception) {
             return response()->json(['error' => 'Invalid credentials provided.']);
         }
 
         /**Membuat user baru dengan email yang tersedia.
          * Secara default passwordnya adalah 123456789.
          */
-        $userCreated = User::firstOrCreate([
-            'email' => $user->getEmail(),
-        ],
-        [
-            'email_verified_at' => now(),
-            'name' => $user->getName(),
-            'status' => true,
-        ]);
+        $userCreated = User::firstOrCreate(
+            [
+                'email' => $user->getEmail(),
+            ],
+            [
+                'email_verified_at' => now(),
+                'name' => $user->getName(),
+                'status' => true,
+            ]
+        );
 
         /**Relasi model providers dengan model Users.
          * Bertujuan untuk membuat record baru pada tabel Providers.
          */
-        $userCreated->providers()->updateOrCreate([
-            'provider' => $provider,
-            'provider_id' => $user->getId(),
-        ],
-        [
-            'avatar' => $user->getAvatar(),
-        ]);
+        $userCreated->providers()->updateOrCreate(
+            [
+                'provider' => $provider,
+                'provider_id' => $user->getId(),
+            ],
+            [
+                'avatar' => $user->getAvatar(),
+            ]
+        );
 
         /**Generate token. */
         $token = $userCreated->createToken('token-name')->plainTextToken;
+
+        /**Ekstrak domain email */
+        $emailDomain = substr(strrchr($user->getEmail(), "@"), 1);
+
+        /**Cek apakah email_domain ada dan is_active bernilai 1 */
+        $autoInviteEmail = AutoInviteEmail::where('email_domain', $emailDomain)
+            ->where('is_active', 1)
+            ->first();
+
+        if ($autoInviteEmail) {
+            /**Membuat invite code */
+            $inviteCode = $this->generateRandomCode(8);
+            $invitation = UserInvitation::create([
+                'email' => $user->email,
+                'invite_code' => $inviteCode,
+                'is_used' => false,
+                'expired_at' => now()->addDays(30),
+            ]);
+
+            /**Mengirim email undangan */
+            self::sendInvitation($invitation);
+        }
 
         /**Response JSON akan mengembalikan data user berupa ID, nama, email, dan token. */
         $response = [
@@ -86,9 +115,47 @@ class SocialiteController extends Controller
      * Jika mau dikembangkan maka bisa diubah-ubah sesuai kebutuhan.
      * Jika penyedia provider tidak valid maka akan ditampilkan pesan error.
      */
-    protected function validateProvider($provider){
+    protected function validateProvider($provider)
+    {
         if (!in_array($provider, ['facebook', 'github', 'google'])) {
             return response()->json(['error' => 'Silakan masuk menggunakan akun Facebook, GitHub, atau Google'], 422);
         }
+    }
+
+    // Send Invite
+    public static function sendInvitation(UserInvitation $user)
+    {
+        try {
+            // Create a new request instance
+            $request = new \Illuminate\Http\Request();
+            $request->replace(['email' => $user->email]);
+
+            // Create an instance of the SendInvitationController
+            $controller = new SendInvitationController();
+
+            // Call the sendInvitation method
+            $response = $controller->sendInvitation($request);
+
+            // Handle response from the controller
+            $responseData = $response->getData();
+        } catch (\Exception $e) {
+        }
+    }
+
+    /**
+     * Generate a random alphanumeric string.
+     *
+     * @param int $length
+     * @return string
+     */
+    protected static function generateRandomCode($length = 8): string
+    {
+        $characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+        $charactersLength = strlen($characters);
+        $randomString = '';
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[rand(0, $charactersLength - 1)];
+        }
+        return $randomString;
     }
 }
